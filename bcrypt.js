ba41fdf28b05c8acc22e4ae264a0a347618dc6f5
LisaKr <lisa.kramarenko@gmail.com>
2018-11-14 17:17:53 +0100

    some styling and deleted redundant comments

--- bcrypt.js
+++ bcrypt.js
@@ -1,12 +1,11 @@
 const bcrypt = require("bcryptjs");
 const { promisify } = require("util");
 
-const genSalt = promisify(bcrypt.genSalt); //generates random salt string
-const hash = promisify(bcrypt.hash); //takes plain text and salt and creates a hash out of it
+const genSalt = promisify(bcrypt.genSalt);
+const hash = promisify(bcrypt.hash);
 const compare = promisify(bcrypt.compare);
 
 exports.hash = function(password) {
-    //because we dont wanna call genSalt by itself, so we are combining them
     return genSalt().then(salt => {
         return hash(password, salt);
     });
